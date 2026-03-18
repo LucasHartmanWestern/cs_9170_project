40dc2a3027f02efd4f6479edcdf6e7b9299a9644
E3than <epigou@gmail.com>
2026-03-18 16:01:31 -0400

    time allocation change1

--- experiment_specs/v17c_bias010_ds030_rl_census_3seeds.sh
+++ experiment_specs/v17c_bias010_ds030_rl_census_3seeds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=v17c_b010_ds030
 #SBATCH --account=def-mcapretz
-#SBATCH --time=09:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mem=3G
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
